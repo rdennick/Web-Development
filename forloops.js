3f66344e0a166c579991b2c3ef83c2b8dd72057f
rdennick <32077800+rdennick@users.noreply.github.com>
2017-09-18 18:31:14 -0400

    Add files via upload

---
+++ forloops.js
@@ -0,0 +1,27 @@
+console.log("Problem 1");
+
+for (var i = -10; i < 20; i++){
+	console.log(i);
+}
+
+console.log("Problem 2");
+
+for (var i = 10; i < 41; i+=2){
+	console.log(i);
+}
+
+console.log("Problem 3");
+
+for (var i = 300; i < 334; i+=1){
+	if (i % 2 === 1){
+		console.log(i)
+	}
+}
+
+console.log("Problem 4");
+
+for (var i = 5; i < 51; i+=1){
+	if (i % 5 === 0 && i % 3 === 0) {
+		console.log(i)
+	}
+}
