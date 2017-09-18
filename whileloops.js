dc9b3380676c827ba038244b7bba1870aeeadd06
rdennick <32077800+rdennick@users.noreply.github.com>
2017-09-18 18:32:10 -0400

    Add files via upload

---
+++ whileloops.js
@@ -0,0 +1,31 @@
+var count = -10;
+
+while (count < 20) {
+	console.log(count);
+	count++;
+}
+
+var even = 10;
+
+while (even < 41) {
+	console.log(even);
+	even +=2;
+}
+
+var odds = 300;
+
+while (odds < 334) {
+	if (odds % 2 !== 0){
+		console.log(odds);
+	}
+	odds++;
+}
+
+var five_three = 5;
+
+while (five_three < 51) {
+	if (five_three % 5 === 0 && five_three % 3 === 0){
+		console.log(five_three)
+	}
+	five_three++;
+}
